feb97d77c9ea088d4708ae2c99cd6630b54f7e5a
Mathieu Dario <mdario.research@gmail.com>
2025-02-25 17:40:18 +0100

    quick fix

--- Note on monitors evaluation metrics/build.sh
+++ Note on monitors evaluation metrics/build.sh
@@ -13,8 +13,7 @@ pdf() {
         --from="markdown+tex_math_single_backslash+tex_math_dollars+raw_tex" \
         --to="latex" \
         --output="${BUILDDIR}/${FILENAME}.pdf" \
-        --pdf-engine="pdflatex" \
-        --bibliography="${CONTENTDIR}/bibliography.bib" 
+        --pdf-engine="pdflatex"
 }
 
 # Allows to call a function based on arguments passed to the script
